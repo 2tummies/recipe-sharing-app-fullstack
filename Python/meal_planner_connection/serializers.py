0ff664ca52c3581b2e4cb08a91833f78759bf15d
Preston Roe <pjroe@sbcglobal.net>
2025-04-14 17:53:48 -0500

    updated user serializer

--- Python/meal_planner_connection/serializers.py
+++ Python/meal_planner_connection/serializers.py
@@ -6,12 +6,7 @@ from .models import *
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields= ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        fields= ['id', 'username', 'date_created', 'birthday']
     
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
