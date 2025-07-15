875478896d2f419ac16f86fecfa385803e00001f
Preston Roe <pjroe@sbcglobal.net>
2025-07-14 20:04:32 -0500

    removed a bit of unwanted code from scratch pad

--- SQL/react_meal_planner_scratch_pad.sql
+++ SQL/react_meal_planner_scratch_pad.sql
@@ -81,6 +81,3 @@ SELECT EXISTS (
 
 SELECT table_name FROM information_schema.tables
 WHERE table_schema='public' AND table_type='BASE TABLE';
-
-SET search_path TO public;
-SELECT * FROM auth_user;
